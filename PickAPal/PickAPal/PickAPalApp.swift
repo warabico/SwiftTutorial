13890c8458ab0e62cb8426c77a3624a7b57e0d24
warabico <121906002+warabico@users.noreply.github.com>
2025-01-01 15:19:26 +0900

    Swift Tutorial: Pick-a-Pal

---
+++ PickAPal/PickAPal/PickAPalApp.swift
@@ -0,0 +1,18 @@
+//
+//  PickAPalApp.swift
+//  PickAPal
+//  
+//  Created by warabico.develop on 2025/01/01
+//  
+//
+
+import SwiftUI
+
+@main
+struct PickAPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
